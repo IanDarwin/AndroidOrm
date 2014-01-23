4386f26e2d54393d6be46c9ea0b692a56807ff38
IanDarwin <ian@darwinsys.com>
2014-01-23 14:12:29 -0500

    Totally whack into an Android project, turning JPASimple from main() into DAO.
    It compiles, haven't tested it yet by any means. APK may be huge.

---
+++ jpa-hibernate/src/main/java/com/example/jpa/JPASimple.java
@@ -0,0 +1,60 @@
+package com.example.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import domain.Person;
+
+public class JPASimple {
+
+	EntityManagerFactory entityMgrFactory;
+	EntityManager entityManager;
+
+	public void init() {
+
+		// These two steps would be done for you
+		// were you running in an EE App Server.
+		// Or just the EntityManager injected if you were using JavaEE or Spring
+		entityMgrFactory = Persistence.createEntityManagerFactory("androidorm");
+		entityManager = entityMgrFactory.createEntityManager();
+	}
+
+	public void persist(Person np) {
+
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+
+		// Create an entity in the database.
+		System.out.println("Trying to save " + np);
+		entityManager.persist(np);
+		transaction.commit();
+
+		int id = np.getId();
+		System.out.println("Created Person with Id " + id);
+	}
+
+	public Person getPersonById(long id) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		Person np = entityManager.find(Person.class, id);
+		transaction.commit();
+		return np;
+	}
+
+	public List<Person> getAllPersons() {
+
+		Query query = entityManager.createQuery("select p from Person p order by p.lastName");
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		@SuppressWarnings("unchecked")
+		List<Person> list = query.getResultList();
+		transaction.commit();
+		return list;
+	}
+
+}
---
+++ jpa-hibernate/src/main/java/com/example/jpa/SavingActivity.java
@@ -0,0 +1,48 @@
+package com.example.jpa;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+import domain.Person;
+
+public class SavingActivity extends Activity {
+	
+	TextView message;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_saving);
+		message = (TextView) findViewById(R.id.saved);
+		final Person p = (Person) getIntent().getSerializableExtra("person");
+		
+		new Thread(new Runnable() {
+			//@Override
+			public void run() {
+				// Write some code here to:                        XXX DELETE THIS LINE WHEN DONE
+				// Save the Person (which might be a Customer subclass, and might have an Address!)
+				
+				// Fetch this copy of "p" back from the database.
+				// XXX DELETE THIS LINE WHEN DONE - get it by Id, probably!
+				Person q = p;
+				
+				String format = getString(R.string.saved);
+				final String toDisplay = String.format(format, q);
+				runOnUiThread(new Runnable() {
+					//@Override
+					public void run() {
+						message.setText(toDisplay);
+					}
+				});
+			}}).start();		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.saving, menu);
+		return true;
+	}
+
+}
